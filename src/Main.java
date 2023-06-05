fc3eba0395b06fe210291fc8b50105194abeedfb
Poteryashka163 <scrat0072@gmail.com>
2023-06-05 06:46:09 +0400

    Добавленны классы

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner registration = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        new User("login","password","password");
         System.out.print("Введите Логин: ");
-        String login = registration.next();
+        String login = scanner.next();
         System.out.print("Введите Пароль: ");
-        String password = registration.next();
+        String password = scanner.next();
         System.out.print("Введите Подтверждение пароля: ");
-        String confirmPassword = registration.next();
+        String confirmPassword = scanner.next();
 
         try {
             CheckCredentials.checkCredentials("java_skypro_go", "D_1hWiKjjP_9", "D_1hWiKjjP_9");
---
+++ src/User.java
@@ -0,0 +1,11 @@
+public class User {
+     String login;
+     String password;
+     String confirmPassword;
+
+    public User (String login, String password,String confirmPassword){
+        this.login = login;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+}
